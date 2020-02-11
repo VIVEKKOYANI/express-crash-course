3e32594291af986a0eb61106e00f77691ae228f7
vivek <vivek.koyani@gmail.com>
2020-02-11 23:52:45 +0530

    filter add

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ app.use(logger);
 
 // Get Single Member
 app.get('/api/member/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if(found){
     res.json(members.filter(member => member.id === parseInt(req.params.id)));
+}else{
+    res.status(400).json({msg: `Member not found ${req.params.id}`});
+}
 });
 
 // Gets All Members
